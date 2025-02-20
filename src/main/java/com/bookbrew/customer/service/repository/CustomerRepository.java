41f9bc65a0779f802141c7c83aaa1cbb6b1b9984
Publio Buiatti Guirelli <publiotester@gmail.com>
2025-02-20 05:50:59 -0300

    :bug: fix: Improvement in services during integration with the web client

--- src/main/java/com/bookbrew/customer/service/repository/CustomerRepository.java
+++ src/main/java/com/bookbrew/customer/service/repository/CustomerRepository.java
@@ -1,5 +1,7 @@
 package com.bookbrew.customer.service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.bookbrew.customer.service.model.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByUserId(Long userId);
 
 }
